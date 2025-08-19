93235f65a748878e8e0670059b3d2319d21125c3
Esther Lyon Delsordo <esther.lyondelsordo@speedgoat.io>
2025-08-19 14:23:27 -0600

    Episode 5: dynamically change styling based on request

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-900">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -7,31 +7,20 @@
     <title>Home Page</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class=>
-<!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
-<!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-900">
-  <body class="h-full">
-  ```
--->
+<body class="h-full">
 <div class="min-h-full">
     <nav class="bg-gray-800/50">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="flex h-16 items-center justify-between">
                 <div class="flex items-center">
                     <div class="shrink-0">
-                        <img src="https://www.speedgoat.io/wp-content/uploads/2018/02/speed-goat-logo.png" alt="Speedgoat Logo" class="h-8 w-12" />
+                        <img src="https://www.speedgoat.io/wp-content/uploads/2018/02/speed-goat-logo.png" alt="Speedgoat Logo" class="h-10 w-full" />
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
-                            <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                            <a href="/" aria-current="page" class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Home</a>
-                            <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                            <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                            <a href="/" aria-current="page" class="{{ request()->is('/')  ? 'bg-gray-950/50 text-white': 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium">Home</a>
+                            <a href="/about" class="{{ request()->is('about')  ? 'bg-gray-950/50 text-white': 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium">About</a>
+                            <a href="/contact" class="{{ request()->is('contact')  ? 'bg-gray-950/50 text-white': 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium">Contact</a>
                         </div>
                     </div>
                 </div>
---
+++ resources/views/Components/nav-link.blade.php
@@ -0,0 +1,5 @@
+@props(['active'])
+<a class="{{ request()->is('/')  ? 'bg-gray-950/50 text-white': 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+   aria-current="{{ request()->is('/')  ? 'page': 'false' }}"
+    {{ $attributes }}
+>{{$slot}}</a>
