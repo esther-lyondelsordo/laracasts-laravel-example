d727981c03711b4dda7f97acf06b5f2d2958b8cc
Esther Lyon Delsordo <esther.lyondelsordo@speedgoat.io>
2025-08-19 11:15:50 -0600

    Commit state of app at the end of episode 3

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Home Page</title>
+</head>
+<body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <nav>
+        <x-nav-link href="/">Home</x-nav-link>
+        <x-nav-link href="/about">About</x-nav-link>
+        <x-nav-link href="/contact">Contact</x-nav-link>
+    </nav>
+
+    {{ $slot }}
+
+</body>
+</html>
+
+
