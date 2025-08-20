4db7d3ca6c32c7aa259257a2d564d7a0e7061617
Esther Lyon Delsordo <esther.lyondelsordo@speedgoat.io>
2025-08-20 17:08:03 -0600

    Updates through Ep 6 at 9:15

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,9 @@
     <x-slot:heading>
         Home Page
     </x-slot:heading>
-    <h1>Hello from the Home Page.</h1>
+
+    @foreach ($jobs as $job)
+        <li><strong>{{ $job['title'] }}</strong> : Pays {{ $job['salary'] }} per year. </li>
+    @endforeach
+
 </x-layout>
